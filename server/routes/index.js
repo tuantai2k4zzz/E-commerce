63a008a691b0c6cb16ad811c0d04e48de42e5efa
tuantai2k4 <buituantai2k4@gmail.com>
2024-11-28 10:20:51 -0500

    #2 Handler error

---
+++ server/routes/index.js
@@ -0,0 +1,15 @@
+const { notFound, errorHandler } = require('../middlewares/errHandler')
+const userRouter = require('./user')
+
+const initRoutes = (app) => {
+    app.use("/api/user", userRouter)
+
+
+
+    app.use(notFound)
+    app.use(errorHandler)
+
+
+}
+
+module.exports = initRoutes
--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const dbConnect = require('./config/dbconnect')
 require('dotenv').config()
+const initRoutes = require("./routes")
 
 const app = express()
 const port = process.env.PORT || 8888
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
+initRoutes(app)
+dbConnect()
 app.use('/', (req, res) => {res.send('SERVER ON!')})
+
 
 app.listen(port, () => {
     console.log('Server running on the port:' + port)
